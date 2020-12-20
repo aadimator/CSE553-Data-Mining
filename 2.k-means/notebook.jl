ee597365bbe593351be53d8f8c0d9330fcb95ea9
Aadam <aadimator@gmail.com>
2020-12-20 20:05:50 +0500

    feat: complete dunn and DBI indices

--- 2.k-means/notebook.jl
+++ 2.k-means/notebook.jl
@@ -28,11 +28,14 @@ begin
 	using LinearAlgebra
 	using Plots
 	using Clustering
+	using Distances
 end
 
 # ╔═╡ c73d5420-4126-11eb-19b3-bf6c04aef086
 md"""
-Assignment 2
+# Data Mining
+## Assignment 2 - K-means Clustering
+### Aadam & Obaidullah (CS1945 & CS1947)
 """
 
 # ╔═╡ 360ab320-411d-11eb-2d11-c9fe7b3137d1
@@ -41,18 +44,16 @@ plotly()
 # ╔═╡ 73936e9e-4116-11eb-0594-1d202bd073cd
 function Kmeans(X, k; max_iters = 300, tol = 1e-5)
     # Reshape 2D array to a 1D array with length of all training examples
-    # where each example is of size (n, ) ie the new array is just a list of example array
     X_array_list = collect(eachrow(X))
-	print(X_array_list)
 
     # Save some info on the incoming data
     N = length(X_array_list)  # Length of all training examples
     n = length(X_array_list[1])  # Length of a single training example
-    distances = zeros(N)  # Empty vector for all training examples. Useful later
+    distances = zeros(N)  # Empty vector for all training examples.
 
     # Step 1: Random initialization
     reps_centroids = [zeros(n) for grp = 1:k]  # Initiate centroids for each
-    labels = rand(1:k, N)  # Randomly assign labels (between 1 to k) to all training examples
+    labels = rand(1:k, N)  # Randomly assign labels to all training examples
 
     J_previous = Inf
 
@@ -69,10 +70,11 @@ function Kmeans(X, k; max_iters = 300, tol = 1e-5)
 
         # Step 3: Update the group labels
         for i = 1:N
-            # compute the distance between each example and the updated representative centroid
-            nearest_rep_distance = [norm(X_array_list[i] - reps_centroids[x]) for x = 1:k]
+            # compute the distance between each example and the updated centroid
+            nearest_rep_distance = 
+			[norm(X_array_list[i] - reps_centroids[x]) for x = 1:k]
 
-            # update distances and label arrays with value and index of closest neighbour
+            # update distances and label arrays 
             # findmin returns the min value & index location
             distances[i], labels[i] = findmin(nearest_rep_distance)
         end;
@@ -85,16 +87,99 @@ function Kmeans(X, k; max_iters = 300, tol = 1e-5)
 
         # Final Step 5: Check for convergence
         if iter > 1 && abs(J - J_previous) < (tol * J)
-            # TODO: Calculate the sum of squares
-
             # Terminate algorithm with the assumption that K-means has converged
             return labels, reps_centroids
-    
         end
 
         J_previous = J
     end
+	return labels, reps_centroids
+end
 
+# ╔═╡ aac40900-41cc-11eb-1103-97d9373bdd65
+function calculate_average_distance(i, X, result)
+	dist = 0
+	center = result.centers[i]
+	for instance in X[:, result.assignments .== i]
+		dist += Euclidean()(instance, center)
+	end
+	return dist/result.counts[i]
+end
+
+# ╔═╡ 12615c80-41c7-11eb-2aed-c1eb96003ff1
+function dbi(X, result)
+	num_of_clusters = length(result.counts)
+	
+	#average distance of all points in a cluster to its center, for each cluster
+    average_centroid_distances = [
+		calculate_average_distance(i, X, result) for i in 1:num_of_clusters]
+	
+	sum_Dij = 0
+	
+	#calculate the sum Dij
+    for i in 1:num_of_clusters     
+        max_Dij = 0
+        
+        #calculate the max Dij
+        for j in 1:num_of_clusters            
+            if i != j
+				
+				#calculate di + dj
+				numerator = 
+				average_centroid_distances[i] + average_centroid_distances[j]
+
+				#calculate dij
+				denominator = 
+				Euclidean()( result.centers[i], result.centers[j] )
+
+				current_Dij = numerator/denominator
+
+				if max_Dij < current_Dij
+					max_Dij = current_Dij
+				end
+			end
+                
+		end
+		sum_Dij += max_Dij
+	end
+    
+    return sum_Dij / num_of_clusters
+end
+
+# ╔═╡ c7001000-42c6-11eb-23d1-314932816f0a
+function big_delta(maskci, distances)
+	maximum(distances[maskci, :][:, maskci])
+end
+
+# ╔═╡ c6ca5a00-42c6-11eb-386e-2f13a99d75b6
+function delta(maskci, maskcj, distances)
+	minimum(distances[maskci, :][:, maskcj])
+end
+
+# ╔═╡ c69d2f80-42c6-11eb-10cf-e70c07e40e97
+function dunn(X, result)
+	n = length(result.counts) # number of clusters
+	
+	distances = pairwise(Euclidean(), X, dims=2)
+	
+	deltas = ones(n, n) * Inf
+	big_deltas = zeros(n)
+	
+	for k in 1:n
+		for l in 1:n
+			if k == l
+				continue
+			end
+            deltas[k, l] = delta(
+				result.assignments .== k, 
+				result.assignments .== l, 
+				distances)
+		end
+        
+        big_deltas[k] = big_delta(result.assignments .== k, distances)
+		
+	end
+	minimum(deltas)/maximum(big_deltas)
 end
 
 # ╔═╡ 9accd320-4117-11eb-0fc4-15e8e85040a8
@@ -114,43 +199,77 @@ features = collect(Matrix(data[:, 2:end-1])');
 
 # ╔═╡ bec93fa0-4119-11eb-026e-0be14d6a5779
 md"""
-Vlaue of **k**: $(@bind k Slider(1:size(UCIData.dataset(selected_dataset))[1]; show_value=true))
+Vlaue of **k**: $(@bind k Slider(2:15; show_value=true))
 """
-
-# ╔═╡ cc1f379e-4127-11eb-375c-d782bcac175d
 # size(UCIData.dataset(selected_dataset))[1]
 
 # ╔═╡ 737b2bb0-4116-11eb-124b-d981032a9638
 result = kmeans(features, k);
 
+# ╔═╡ 725d2e10-42c2-11eb-3ee7-4ba089ebb440
+md"""
+### Davies Bouldin Index: $(round(dbi(features, result), digits=5))
+### Dunn Index : $(round(dunn(features, result), digits=5))
+"""
+
 # ╔═╡ ce4134a0-4124-11eb-2a7c-9761e81b2064
 md"""
 ## Plot Clusters
-Feature 1: $(@bind feature1 Select(string.(collect(1:size(features)[1]))))
-Feature 2: $(@bind feature2 Select(string.(collect(1:size(features)[1])));)
+Feature $(@bind feature1 Select(string.(collect(1:size(features)[1])))) vs.
+Feature $(@bind feature2 Select(string.(collect(1:size(features)[1])));)
 """
 
 # ╔═╡ 73454ea0-4116-11eb-0938-f9a06f3dbaf8
-scatter(features[parse(Int,feature1), :], features[parse(Int, feature2), :], 
-        marker_z = result.assignments, 
-        color =:gist_rainbow, legend = false)
+begin
+	theme(:dark)
+	scatter(
+		features[parse(Int,feature1), :], 
+		features[parse(Int, feature2), :],
+		marker_z = result.assignments, 
+		markershape = :diamond,
+		markersize = 3,
+		xlabel="Feature $(feature1)", 
+		ylabel="Feature $(feature2)",
+		color = :seaborn_bright, 
+		legend = false)
+	scatter!(
+		result.centers[parse(Int,feature1), :], 
+		result.centers[parse(Int, feature2), :],
+		markersize = 6,
+		marker_z = result.centers',
+		color = :temperaturemap
+	)
+end
 
-# ╔═╡ 548ac220-4124-11eb-0da7-b96eb51b89d9
-
+# ╔═╡ 6cd3fe00-41c4-11eb-2e84-376a14af7398
+md"""
+### Cluster Centers
+$(
+df = DataFrame(hcat(result.centers', result.counts));
+last_col_name = names(df)[end];
+df[!,last_col_name] = convert.(Int,df[:,last_col_name]);
+rename!(df, last_col_name => :NodeCount)
+)
+"""
 
 # ╔═╡ Cell order:
-# ╠═c73d5420-4126-11eb-19b3-bf6c04aef086
+# ╟─c73d5420-4126-11eb-19b3-bf6c04aef086
 # ╟─85d5a830-4116-11eb-1479-630b53b50e94
 # ╠═4862c2d0-4116-11eb-3f26-1bdefcb9ce3d
 # ╟─360ab320-411d-11eb-2d11-c9fe7b3137d1
 # ╠═73936e9e-4116-11eb-0594-1d202bd073cd
+# ╠═aac40900-41cc-11eb-1103-97d9373bdd65
+# ╠═12615c80-41c7-11eb-2aed-c1eb96003ff1
+# ╠═c7001000-42c6-11eb-23d1-314932816f0a
+# ╠═c6ca5a00-42c6-11eb-386e-2f13a99d75b6
+# ╠═c69d2f80-42c6-11eb-10cf-e70c07e40e97
 # ╟─9accd320-4117-11eb-0fc4-15e8e85040a8
 # ╟─8b43a9a0-4118-11eb-127d-d57dcea189e1
 # ╠═73c57b20-4116-11eb-3dba-01c9f7b91fa7
 # ╠═5cddc6a0-411c-11eb-2380-1b9442938a9b
-# ╠═bec93fa0-4119-11eb-026e-0be14d6a5779
-# ╠═cc1f379e-4127-11eb-375c-d782bcac175d
+# ╟─bec93fa0-4119-11eb-026e-0be14d6a5779
 # ╠═737b2bb0-4116-11eb-124b-d981032a9638
+# ╠═725d2e10-42c2-11eb-3ee7-4ba089ebb440
 # ╟─ce4134a0-4124-11eb-2a7c-9761e81b2064
-# ╠═73454ea0-4116-11eb-0938-f9a06f3dbaf8
-# ╠═548ac220-4124-11eb-0da7-b96eb51b89d9
+# ╟─73454ea0-4116-11eb-0938-f9a06f3dbaf8
+# ╟─6cd3fe00-41c4-11eb-2e84-376a14af7398
