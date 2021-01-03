238e110b0db462ff65b0c38f8e6c92948b0c1f42
Aadam <aadimator@gmail.com>
2021-01-03 07:31:45 +0500

    initial

---
+++ 4.semester-project/notebook.jl
@@ -0,0 +1,54 @@
+### A Pluto.jl notebook ###
+# v0.12.17
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 8d2a2140-4d6b-11eb-1ed0-5dfeba2603af
+begin
+	using Pkg
+    Pkg.activate(pwd())
+end
+
+# ╔═╡ a4778db2-4d6b-11eb-0e97-c368743a47ea
+md"""
+# Data Mining
+## Semester Project - Clustering
+### Aadam - CS1945
+"""
+
+# ╔═╡ b9452d10-4d6b-11eb-1074-83294cc14eec
+
+
+# ╔═╡ ba80451e-4d6b-11eb-0949-6de777cd3780
+
+
+# ╔═╡ ba9d4300-4d6b-11eb-07f8-9d41ace0af45
+
+
+# ╔═╡ bab36310-4d6b-11eb-08b9-4b21af254fc3
+
+
+# ╔═╡ bac95c10-4d6b-11eb-0800-879a9bf054ef
+
+
+# ╔═╡ bae0b4a0-4d6b-11eb-21bd-47166209512e
+
+
+# ╔═╡ baf68690-4d6b-11eb-3151-b5e91d478309
+
+
+# ╔═╡ bb0fb3e0-4d6b-11eb-2f60-e9a2de669396
+
+
+# ╔═╡ Cell order:
+# ╟─8d2a2140-4d6b-11eb-1ed0-5dfeba2603af
+# ╠═a4778db2-4d6b-11eb-0e97-c368743a47ea
+# ╠═b9452d10-4d6b-11eb-1074-83294cc14eec
+# ╠═ba80451e-4d6b-11eb-0949-6de777cd3780
+# ╠═ba9d4300-4d6b-11eb-07f8-9d41ace0af45
+# ╠═bab36310-4d6b-11eb-08b9-4b21af254fc3
+# ╠═bac95c10-4d6b-11eb-0800-879a9bf054ef
+# ╠═bae0b4a0-4d6b-11eb-21bd-47166209512e
+# ╠═baf68690-4d6b-11eb-3151-b5e91d478309
+# ╠═bb0fb3e0-4d6b-11eb-2f60-e9a2de669396
